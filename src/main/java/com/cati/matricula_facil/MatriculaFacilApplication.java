cbc737012e7d3f38e58880751d6ab65fd3e65e69
carloskiyoto <dudu.kiyoto@gmail.com>
2026-06-06 13:16:16 -0300

    feat: basic creation of docker-compose and application.proprieties

--- src/main/java/com/cati/matricula_facil/MatriculaFacilApplication.java
+++ src/main/java/com/cati/matricula_facil/MatriculaFacilApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class MatriculaFacilApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MatriculaFacilApplication.class, args);
 	}
